645acb2801bd9ade21e33a91e85bc779883034f8
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-05-21 22:37:32 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: [
+            '.jsx',
+            '.js',
+            '.tsx',
+            '.ts'
+        ],
+    },
+    entry: {
+        app: './client'
+    },
+    modules: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            }
+        ]
+    },
+    plugins: [
+
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+
+    }
+}
